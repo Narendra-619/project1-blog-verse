d108e5cfa0412ec2a2d06caea3dec5440ba6182f
Narendra-619 <kknarendra619@gmail.com>
2025-09-06 12:33:43 +0530

    done something

--- sample.js
+++ sample.js
@@ -1,1 +1,16 @@
 console.log("Admin sample cooked.");
+let name="Thomas Shelby";
+console.log(`Hello, ${name}`);
+let num=18+18
+console.log(`The sum of 18+18 is ${num}`);
+console.log(typeof(num))
+let islogggedIn=true;
+console.log(islogggedIn)
+let x;
+console.log(typeof(x))
+let y=null;
+console.log(typeof(y))
+let arr=[1,2,3,4,5];
+console.log(arr)
+let person={name:"Narendra",age:21,city:"Bangalore"};
+console.log(person)
