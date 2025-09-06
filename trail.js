1d838039425d198f1a7ed21e2474b285a40b83c7
Narendra-619 <kknarendra619@gmail.com>
2025-09-06 14:59:03 +0530

    Did some practice

---
+++ trail.js
@@ -0,0 +1,31 @@
+let x=7,y=5,z=10;
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)        
+console.log(x**y)
+console.log(++x)
+console.log(--y)    
+z+=x+y
+console.log(z)
+console.log(z>x && z>y)
+console.log(z>x || z<y)
+console.log(!(z>x && z>y))  
+k="bradley coooper"
+l=k+z
+console.log(l)
+f='mikel'
+g='guardiola'
+console.log(f+g)
+console.log(`${f} ${g}`)
+let messaage=`Hello ${f}, welcome to the world of JavaScript!`
+console.log(messaage)
+let count=6;
+console.log(count++)
+console.log(count)
+console.log(++count)
+console.log(count)
+console.log(count--)
+console.log(count)
+console.log(--count)    
